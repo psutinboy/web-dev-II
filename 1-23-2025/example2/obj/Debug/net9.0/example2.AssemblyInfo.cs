2bea69af0124467d2d7a7a1f15821a513d86dc12
Psutinboy <grantledbetter12@gmail.com>
2025-02-27 08:01:44 -0600

    2-25-2025

--- 1-23-2025/example2/obj/Debug/net9.0/example2.AssemblyInfo.cs
+++ 1-23-2025/example2/obj/Debug/net9.0/example2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("example2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60c4fdea7d1ac033337b11e140f37db1b21013ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e148e447214e9d6733dc0bc774ee42c90d7bd84")]
 [assembly: System.Reflection.AssemblyProductAttribute("example2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("example2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
